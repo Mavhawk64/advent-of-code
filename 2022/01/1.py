9bdbfcf71257c8f25e4828d131529a7afdcd5e2f
Berkland, Maverick <Maverick.Berkland@dell.com>
2023-11-28 10:56:16 -0500

    Added an additional comment to the starter files

--- 2022/01/1.py
+++ 2022/01/1.py
@@ -1,5 +1,2 @@
 # Day 1, Star 1
-f = open('input.txt', 'r')
-g = f.read().split('\n\n')
-print(max([sum([int(x) for x in i.split('\n')]) for i in g]))
-f.close()
+print(max([sum([int(x) for x in i.split('\n')]) for i in open('input.txt', 'r').read().split('\n\n')]))
--- generate_folders.py
+++ generate_folders.py
@@ -7,11 +7,13 @@ if not os.path.isdir(f"./{year}"):
 
 os.chdir(f"./{year}")
 
-for i in range(1,26):
+for i in range(7,26):
 	if not os.path.isdir(f"./{i:02d}"):
 		os.mkdir(f"./{i:02d}")
 	f1 = open(f"./{i:02d}/1.py","w")
+	f1.write(f"# Day {i}, Star 1")
 	f2 = open(f"./{i:02d}/2.py","w")
+	f2.write(f"# Day {i}, Star 2")
 	f3 = open(f"./{i:02d}/input.txt","w")
 	f1.close()
 	f2.close()
