94e23ffad71c4ce4ad81cf7b9a836b210a07cfec
Maverick <mavberk@gmail.com>
2023-12-01 22:46:09 -0500

    Completed 2023-12-01

--- 2023/01/1.py
+++ 2023/01/1.py
@@ -1,1 +1,28 @@
 # Day 1, Star 1
+
+def parse_int(s):
+    dec = 0
+    x = 0
+    for i in range(1, len(s)+1):
+        print(s[-i], s[-i] in ['1', '2', '3', '4',
+              '5', '6', '7', '8', '9', '0'], x, dec)
+        if s[-i] in ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']:
+            x += 10**dec * int(s[-i])
+            dec += 1
+            break
+
+    for i in range(0, len(s)):
+        if s[i] in ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']:
+            x += 10**dec * int(s[i])
+            break
+    print(x)
+    return x
+
+
+with open("input.txt") as f:
+    g = f.read().split('\n')
+    s = 0
+    for i in g:
+        s += parse_int(i)
+    print(s)
+    f.close()
--- 2023/01/2.py
+++ 2023/01/2.py
@@ -1,1 +1,31 @@
 # Day 1, Star 2
+def parse_int(s):
+    alphabet = ['zero', 'one', 'two', 'three', 'four',
+                'five', 'six', 'seven', 'eight', 'nine', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    word = ''
+    words = []
+    i = 0
+    while i < len(s):
+        word += s[i]
+        # print(i, word)
+        if len([j for j in alphabet if j.startswith(word)]) == 0:
+            i -= len(word) - 1
+            # print(word, i, s[i], "not in alphabet")
+            word = ''
+        if s[i] in alphabet or word in alphabet:
+            words.append(word)
+            i -= len(word) - 1
+            word = ''
+        i += 1
+    print(words)
+    return 10*(alphabet.index(words[0]) % 10) + (alphabet.index(words[-1]) % 10)
+
+
+with open("input.txt") as f:
+    g = f.read().split('\n')
+    s = 0
+    for i in g:
+        # print(i)
+        s += parse_int(i)
+    print(s)
+    f.close()
