e050c26715fec99e875803f8aa7b8a5f808afdaa
Maverick <mavberk@gmail.com>
2023-12-04 00:19:10 -0500

    Completed Day 2

--- 2023/02/1.py
+++ 2023/02/1.py
@@ -1,1 +1,25 @@
 # Day 2, Star 1
+def do_work(sets, g_id):
+    for set in sets:
+        rolls = set.split(", ")
+        for roll in rolls:
+            amt = int(roll.split(" ")[0])
+            color = roll.split(" ")[1]
+            if color not in reqs or amt > reqs[color]:
+                print("Game {} is invalid".format(g_id))
+                return g_id
+    return 0
+
+
+with open("input.txt", "r") as f:
+    id_sum = 5050
+    reqs = {"red": 12, "green": 13, "blue": 14}
+    g = f.read().split("\n")
+    for game in g:
+        g_id = int(game.split(":")[0][len("Game "):])
+        print(g_id)
+        sets = game.split(": ")[1].split("; ")
+        id_sum -= do_work(sets, g_id)
+
+    print(id_sum)
+    f.close()
--- 2023/02/2.py
+++ 2023/02/2.py
@@ -1,1 +1,24 @@
 # Day 2, Star 2
+def do_work(sets, g_id):
+    min_colors = {}
+    for set in sets:
+        rolls = set.split(", ")
+        for roll in rolls:
+            amt = int(roll.split(" ")[0])
+            color = roll.split(" ")[1]
+            if color not in min_colors or amt > min_colors[color]:
+                min_colors[color] = amt
+    print(min_colors)
+    return min_colors["red"] * min_colors["green"] * min_colors["blue"]
+
+
+with open("input.txt", "r") as f:
+    g = f.read().split("\n")
+    power_sum = 0
+    for game in g:
+        g_id = int(game.split(":")[0][len("Game "):])
+        print(g_id)
+        sets = game.split(": ")[1].split("; ")
+        power_sum += do_work(sets, g_id)
+    print(power_sum)
+    f.close()
